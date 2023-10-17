4ac67239fd2d93dbd8e6ae709f36dd23fbf4304c
PengDeng <peng.deng@cyberconnect.me>
2023-10-17 12:35:33 +0800

    deploy_multicall

--- script/Tmp.s.sol
+++ script/Tmp.s.sol
@@ -22,27 +22,6 @@ import "forge-std/console.sol";
 contract TempScript is Script {
     function run() external {
         vm.startBroadcast();
-
-        // CyberAccountFactory fac = CyberAccountFactory(
-        //     0x70Efb7410922159Dd482CD848fB4a7e8c266F95c
-        // );
-        // console.log(address(fac.kernelTemplate()));
-        // console.log(address(fac.nextTemplate()));
-
-        // console.log(
-        //     fac.getAccountAddress(
-        //         IKernelValidator(0x415F0433b90215817a070511083C40aE3876EDE8),
-        //         hex"2E0446079705B6Bacc4730fB3EDA5DA68aE5Fe4D",
-        //         uint256(0)
-        //     )
-        // );
-        // IEntryPoint(0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789).StakeInfo(
-        //     0x870fe151D548A1c527C3804866FaB30ABf28ED17
-        // );
-        TokenReceiver(0xcd97405Fb58e94954E825E46dB192b916A45d412).depositTo{
-            value: 0.01 ether
-        }(0x6651Fa0D314C5b5818d60320C6809f6D5254Fdb9);
-
         vm.stopBroadcast();
     }
 }
