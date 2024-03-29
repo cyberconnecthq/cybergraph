5838fc8a54664e9e60d7f8e19e5710eb719fa156
yikedu <duyike2021@gmail.com>
2024-03-29 16:41:48 +0800

    feat: work in crypto nft

---
+++ src/periphery/WorkInCryptoNFT.sol
@@ -0,0 +1,197 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity 0.8.14;
+
+import { AccessControl } from "openzeppelin-contracts/contracts/access/AccessControl.sol";
+import { ERC721 } from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import { ERC721Enumerable } from "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import { ERC721Burnable } from "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import { ERC721Pausable } from "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+
+import { EIP712 } from "../base/EIP712.sol";
+import { IWorkInCryptoNFT } from "../interfaces/IWorkInCryptoNFT.sol";
+import { DataTypes } from "../libraries/DataTypes.sol";
+
+/**
+ * @title WorkInCryptoNFT
+ * @author CyberConnect
+ * @notice This contract is the NFT for IWorthInCrypto Campaign.
+ */
+contract WorkInCryptoNFT is
+    AccessControl,
+    EIP712,
+    ERC721,
+    ERC721Enumerable,
+    ERC721Burnable,
+    ERC721Pausable,
+    IWorkInCryptoNFT
+{
+    /*//////////////////////////////////////////////////////////////
+                            STORAGE
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Token URI prefix.
+     */
+    string public baseTokenURI;
+
+    /**
+     * @notice The address of the signer.
+     */
+    address internal _signer;
+
+    /**
+     * @notice Nonces for each address.
+     */
+    mapping(address => uint256) internal _nonces;
+
+    /*//////////////////////////////////////////////////////////////
+                                CONSTANTS
+    //////////////////////////////////////////////////////////////*/
+
+    bytes32 internal constant _OPERATOR_ROLE =
+        keccak256(bytes("OPERATOR_ROLE"));
+
+    bytes32 internal constant _MINTER_ROLE = keccak256(bytes("MINTER_ROLE"));
+
+    bytes32 internal constant _MINT_TYPEHASH =
+        keccak256(
+            "mint(address to,uint256 tokenId,uint256 nonce,uint256 deadline)"
+        );
+
+    /*//////////////////////////////////////////////////////////////
+                                CONSTRUCTORS 
+    //////////////////////////////////////////////////////////////*/
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        address owner
+    ) ERC721(name, symbol) {
+        _signer = owner;
+        _grantRole(DEFAULT_ADMIN_ROLE, owner);
+        _grantRole(_OPERATOR_ROLE, owner);
+        _grantRole(_MINTER_ROLE, owner);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            PUBLIC VIEW
+    //////////////////////////////////////////////////////////////*/
+
+    function mint(
+        address to,
+        uint256 tokenId
+    ) public virtual onlyRole(_MINTER_ROLE) {
+        _mint(to, tokenId);
+    }
+
+    function mintWithSig(
+        address to,
+        uint256 tokenId,
+        DataTypes.EIP712Signature calldata sig
+    ) public {
+        _requiresExpectedSigner(
+            _hashTypedDataV4(
+                keccak256(
+                    abi.encode(
+                        _MINT_TYPEHASH,
+                        to,
+                        tokenId,
+                        _nonces[to]++,
+                        sig.deadline
+                    )
+                )
+            ),
+            _signer,
+            sig
+        );
+
+        _mint(to, tokenId);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            DEFAULT_ADMIN ONLY
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Set the signer address.
+     */
+    function setSigner(address signer) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _signer = signer;
+        _grantRole(_MINTER_ROLE, signer);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            OPERATOR ONLY
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Pauses all token transfers.
+     */
+    function pause() external onlyRole(_OPERATOR_ROLE) {
+        _pause();
+    }
+
+    /**
+     * @notice Unpauses all token transfers.
+     */
+    function unpause() external onlyRole(_OPERATOR_ROLE) {
+        _unpause();
+    }
+
+    /**
+     * @notice Set the base token uri.
+     */
+    function setBaseTokenURI(
+        string calldata uri
+    ) external onlyRole(_OPERATOR_ROLE) {
+        baseTokenURI = uri;
+        emit BaseTokenURISet(uri);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            EIP-712 OVERRIDES
+    //////////////////////////////////////////////////////////////*/
+
+    function _domainSeparatorName()
+        internal
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        return "WorkInCryptoNFT";
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            ERC-721 OVERRIDES
+    //////////////////////////////////////////////////////////////*/
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 firstTokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable, ERC721Pausable) {
+        super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721) {
+        super._burn(tokenId);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            ERC-165 OVERRIDES
+    //////////////////////////////////////////////////////////////*/
+
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        override(ERC721, AccessControl, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
